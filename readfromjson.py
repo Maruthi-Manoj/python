a4291466a4e35ce0eeb27726f4e76d6dfa53e24d
Maurthi-Manoj <maruthi.manoj.edu@gmail.com>
2018-06-15 20:38:10 +0000

    changed list to dictionary

--- readfromjson.py
+++ readfromjson.py
@@ -1,39 +1,45 @@
 import json
 import re
+def convertToMB(size):
+    ele = size.split(" ") 
+    number = float(ele[0])
+    unit = ele[1]
+    temp = 0
+    if unit == 'bytes':  
+        temp = number/(1024*1024)
+    
+    elif unit == 'KB':
+        temp = number/1024           
+    
+    elif unit == 'MB':   
+        temp = number                                                                                            
+    
+    elif unit == 'GB': 
+        temp = number*1024
+
+    elif unit == 'TB':
+        temp = number*1024*1024
+    
+    return temp
+
 
 with open('temp1.json','r') as f:
-    total = []
-    tables=[]
+    tablesandsizes= {}
     read_data = json.load(f,strict=False)
     servers = read_data.keys()
     for server in servers:
         stdout = read_data[server]["stdout"]
-        if len(tables) == 0:
-            pattern = re.compile("Table: (.*)") #match everything after "Table: " except new line
-            tables = pattern.findall(stdout) 
-            print(len(tables))
+        pattern = re.compile("Table: (.*)") #match everything after "Table: " except new line
+        tables = pattern.findall(stdout) 
+        for table in tables:
+            if table not in tablesandsizes.keys():
+                tablesandsizes[table]=0
         pattern1 = re.compile("Space used \(total\): (.*)") #match everything after "Space used (total): " except new line
         size = pattern1.findall(stdout)
-        if len(total) == 0:
-            for i in range(0, len(size)):
-                total.insert(i,0)
-            print(len(total))
-        for i in range(0, len(size)):
-            ele = size[i].split(" ")
-            number = float(ele[0])
-            if ele[1] == 'bytes':
-               temp = number/(1024*1024)
-            elif ele[1] == 'MB':
-                temp = number
-            elif ele[1] == 'GB':
-                temp = number*1024
-            total[i] = total[i]+temp
-
-    for i in range(0,len(total)):
-        print("{0}   {1}MB".format(tables[i].encode("utf-8"), total[i]))
-
-             
-
-
-
-            
+        localtablesandsizes={}
+        for i in range(0,len(tables)):
+            localtablesandsizes[tables[i]]= convertToMB(size[i])
+        for i in localtablesandsizes.keys():
+            tablesandsizes[i] = tablesandsizes[i] + localtablesandsizes[i]
+    for k,v in tablesandsizes.items():
+        print("{0}  {1}".format(k,v))
